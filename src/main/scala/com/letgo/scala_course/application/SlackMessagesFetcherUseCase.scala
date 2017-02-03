f0783105a414498e4461984e60df00b45fc88002
Sergio Rodriguez <sergio.rodriguez@letgo.com>
2017-02-03 16:45:00 +0100

    Lesson 1 done

--- src/main/scala/com/letgo/scala_course/application/SlackMessagesFetcherUseCase.scala
+++ src/main/scala/com/letgo/scala_course/application/SlackMessagesFetcherUseCase.scala
@@ -5,13 +5,41 @@ import scala.concurrent.{ExecutionContext, Future}
 import com.letgo.scala_course.domain.{ChannelId, Message, SlackClient}
 
 class SlackMessagesFetcherUseCase(slackClient: SlackClient)(implicit ec: ExecutionContext) {
-  val numberOfApiCalls: Int = 1
+  var numberOfApiCalls: Int = 0
 
   var cache: Option[Future[Seq[Message]]] = None
 
-  def fetch(channelName: ChannelId): Future[Seq[Message]] =
+  def fetch(channelName: ChannelId): Future[Seq[Message]] = {
+    numberOfApiCalls = numberOfApiCalls + 1
     slackClient.fetchChannelMessages(channelName)
+  }
 
-  def fetchWithCache(channelName: ChannelId): Future[Seq[Message]] =
-    slackClient.fetchChannelMessages(channelName)
+  def fetchWithCacheSergio(channelName: ChannelId): Future[Seq[Message]] = {
+    if (cache.isDefined) {
+      cache.get
+    } else {
+      val result = fetch(channelName)
+      cache = Some(result)
+      result
+    }
+  }
+
+  def fetchWithCacheImanol(channelName: ChannelId): Future[Seq[Message]] = {
+    cache.getOrElse {
+      val result = fetch(channelName)
+      cache = Some(result)
+      result
+    }
+  }
+
+  def fetchWithCache(channelName: ChannelId): Future[Seq[Message]] = {
+    cache match {
+      case None => {
+        val result = fetch(channelName)
+        cache = Some(result)
+        result
+      }
+      case Some(a) => a
+    }
+  }
 }
