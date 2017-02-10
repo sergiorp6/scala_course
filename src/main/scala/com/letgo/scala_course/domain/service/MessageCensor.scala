27291b003c5aed9fbc0c4d2dcaae39dc5eba2cd0
Sergio Rodriguez <sergio.rodriguez@letgo.com>
2017-02-10 17:38:59 +0100

    lesson 2

--- src/main/scala/com/letgo/scala_course/domain/service/MessageCensor.scala
+++ src/main/scala/com/letgo/scala_course/domain/service/MessageCensor.scala
@@ -4,7 +4,24 @@ import com.letgo.scala_course.domain.Message
 
 class MessageCensor(forbiddenKeywords: Set[String]) {
 
-  private val filterRule: Message => Message = ???
+//  private val filterRuleSergio: Message => Message = { message =>
+//
+//    var returnValue = message
+//
+//    forbiddenKeywords.foreach(forbiddenWord =>
+//        returnValue = Message(returnValue.text.replaceAll(forbiddenWord, ""))
+//    )
+//
+//    Message(returnValue.text.replaceAll("  ", " ").trim)
+//  }
+
+  private val filterRule: Message => Message = { msg =>
+    Message(msg.text.split(" ").filterNot(containsForbiddenWord).mkString(" "))
+  }
+
+  def containsForbiddenWord(word: String): Boolean = {
+    forbiddenKeywords.contains(word)
+  }
 
   def filterMessages(messages: Seq[Message]): Seq[Message] = messages.map(filterRule)
 }
